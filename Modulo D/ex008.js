a962fbce8b9b3573b0472ac120531727036b1d7d
Lucas Valadão Bispo <64443466+LucasValada@users.noreply.github.com>
2025-07-04 15:41:04 -0300

    Add files via upload

---
+++ Modulo D/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 70.4;
+console.log(`a velocidade do carro estava a ${velocidade}Km/h`);
+if (velocidade > 80) {
+  //condição simples
+  console.log("voce ultrapassou a velocidade maxima. multado!");
+} //else {
+//   console.log("voce ta safe.");
+// }
+console.log("use cinto de segurança!");
