41cc66bf16624eaa55025f573d0c3d7d442ba6cd
Lucas Valadão <valadaolucas6@gmail.com>
2025-07-07 08:51:03 -0300

    aulas de repetiçoes Parte 1

---
+++ Módulo E/ambiente.js
@@ -0,0 +1,18 @@
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+
+var contador = 1
+while(contador <= 10){
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+var c = 1 
+do {
+    console.log(`Passo do ${c}`)
+    c++
+} while (c <= 10)
